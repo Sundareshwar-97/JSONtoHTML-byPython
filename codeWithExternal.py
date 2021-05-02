d312a7d4e003219d3011008d2ee328b212a8714f
Sundareshwar97 <65240175+Sundareshwar97@users.noreply.github.com>
2021-05-02 17:31:51 +0530

    code for converting JSON to HTML

--- codeWithExternal.py
+++ codeWithExternal.py
@@ -1,10 +1,8 @@
-from json2table import *
+from json2html import *
 import json
 with open("data.json", "r") as f:
     json_object = json.loads(f.read())
-    build_direction = "LEFT_TO_RIGHT"
-    table_attributes = {"style": "width:100%"}
-    input = convert(
-        json_object, build_direction=build_direction, table_attributes=table_attributes
-    )
-print(input)
+
+#print(json_object)
+html = json2html.convert(json = json_object)
+print(html)
